8a12582bbd991ae877968b9e9b0d723b0a543fa8
RICARDO JUTURI <Ricardojuturi2@gmail.com>
2022-04-05 21:45:37 -0300

    concesionaria-terminado

---
+++ controllers/indexController.js
@@ -0,0 +1,14 @@
+let { getSucursales } = require('../data/dataBase.js')
+
+
+let controller = {
+    index: function(req, res) {
+        res.render('index', { 
+            sucursales: getSucursales 
+        });
+    }
+}
+
+module.exports = controller
+
+
